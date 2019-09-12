f730854362d76f07c745ca7a75613fb218fb0a4c
botros <botros.nawakil@outlook.de>
2019-09-12 11:31:05 +0200

    dd

--- cachier.js
+++ cachier.js
@@ -1,6 +1,18 @@
 
 
-function Artikelnamen(name){
- var preis = 
-  
+function artikelName (name){
+    var prise
+    if( name == "banane"){
+        prise = 1.19
+    }
+    return prise
 }
+
+
+function artikelMenge(name,menge){
+    prise =  artikelName(name) * menge
+    gesamtSumme += prise
+   return gesamtSumme
+    
+}
+

09e2f1f8e8ce1ccf0e379cc22ecae8ed8218db5f
botros <botros.nawakil@outlook.de>
2019-09-12 11:30:55 +0200

    dd

--- register.js
+++ register.js
@@ -11,19 +11,5 @@ function func (gbetrag){
     
 }
 
-function artikelName (name){
-    var prise
-    if( name == "banane"){
-        prise = 1.19
-    }
-    return prise
-}
 
-
-function artikelMenge(name,menge){
-    prise =  artikelName(name) * menge
-    gesamtSumme += prise
-    return gesamtSumme
-    
-}
 
